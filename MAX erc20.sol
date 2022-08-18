58272f170ed411d8d5a0f2af3dc090e65f6b8f72
Nuthan Prasad <nuthan1159@gmail.com>
2022-08-18 08:32:18 +0530

    Create MAX erc20.sol

---
+++ MAX erc20.sol
@@ -0,0 +1,80 @@
+//SPDX-License-Identifier: GPL-3.0
+
+pragma solidity ^0.8.16;
+
+interface IERC20 {
+    function totalSupply() external view returns (uint);
+
+    function balanceOf(address account) external view returns (uint);
+
+    function transfer(address recipient, uint amount) external returns (bool);
+
+    function allowance(address owner, address spender) external view returns (uint);
+
+    function approve(address spender, uint amount) external returns (bool);
+
+    function transferFrom( address sender,address recipient,uint amount) external returns (bool);
+
+    event Transfer(address indexed from, address indexed to, uint value);
+    event Approval(address indexed owner, address indexed spender, uint value);
+}
+
+contract MAX is IERC20{
+
+    string public name = "MAX";
+    string public symbol = "MAX";
+    uint public decimals = 0;
+    uint public override totalSupply;
+
+    address public founder;
+    mapping(address => uint)  balances;
+    
+    mapping(address => mapping(address => uint)) allowed;
+
+
+    constructor(){
+        totalSupply = 1000000;
+        founder = msg.sender;
+        balances[founder] = totalSupply;
+    }
+
+    function balanceOf(address account) public view override returns (uint){
+        return balances[account];
+    } 
+
+    function transfer(address recipient, uint amount) public override returns (bool){
+        require(balances[msg.sender] > amount,"not sufficient balance");
+
+        balances[msg.sender] -= amount;
+        balances[recipient] += amount;
+
+        emit Transfer(msg.sender, recipient, amount);
+        return true;
+    }
+
+    function allowance(address owner, address spender) public override view returns (uint){
+
+        return allowed[owner][spender];
+    }
+
+    function approve(address spender, uint amount) public override returns (bool){
+        require(balances[msg.sender] >= amount);
+
+        allowed[msg.sender][spender] = amount;
+        emit Approval(msg.sender,spender, amount);
+
+        return true;
+    }
+
+    function transferFrom(address sender,address recipient,uint amount) public override returns (bool){
+        require(allowed[sender][recipient] >= amount && amount > 0);
+        require(balances[sender] >= amount);
+
+        balances[sender] -= amount;
+        balances[recipient] += amount;
+
+        allowed[sender][recipient] -=amount;
+        return true;
+    }
+
+}
